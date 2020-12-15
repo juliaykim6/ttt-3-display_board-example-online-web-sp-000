17c6d09461488557875228c9a61314a3e727d020
Julia Kim <juliaykim6@gmail.com>
2020-12-15 01:32:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  puts "This is a tic tac toe board. "
+  puts "This is a tic tac toe board."
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
